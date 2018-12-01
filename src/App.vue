ca4b1be9613f7416019355a270d686fa7fc2f688
Dmitriy Garbuz <dmigar@gmail.com>
2018-12-01 20:03:13 +0300

    Init

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+<main id="app">
+  <CarBrandsBlock class="app_CarBrandsBlock" />
+</main>
+</template>
+
+<style>
+@font-face {
+  font-family: Ubuntu Condensed;
+  src: url("./assets/Ubuntu-C.ttf");
+}
+
+html,
+body {
+  margin: 0;
+  padding: 0;
+}
+
+.app_CarBrandsBlock {
+  margin: auto;
+}
+</style>
+
+
+<script>
+import CarBrandsBlock from "./CarBrandsBlock";
+export default {
+  name: "app",
+  components: {
+    CarBrandsBlock
+  }
+};
+</script>
