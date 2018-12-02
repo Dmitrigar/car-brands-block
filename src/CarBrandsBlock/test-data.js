4f4c10eff57429fe0d98f68d5597e52404607fba
Dmitriy Garbuz <dmigar@gmail.com>
2018-12-02 17:02:21 +0300

    Add test data

---
+++ src/CarBrandsBlock/test-data.js
@@ -0,0 +1,69 @@
+export default [
+  {
+    brand: "Kia",
+    car_count: 92,
+    first_image: "",
+    id: 1,
+    models: [
+      { count: 11160, name: "Optima" },
+      { count: 28587, name: "Rio" },
+      { count: 27455, name: "Sorento" },
+      { count: 13448, name: "Cerato" },
+      { count: 21046, name: "Sorento Prime" }
+    ],
+    second_image: ""
+  },
+  {
+    brand: "Hyundai",
+    car_count: 411,
+    first_image: "",
+    id: 2,
+    models: [
+      { count: 17763, name: "Tucson" },
+      { count: 28951, name: "Elantra" },
+      { count: 7509, name: "Sorento" },
+      { count: 26782, name: "i40" },
+      { count: 5647, name: "i35" }
+    ],
+    second_image: ""
+  },
+  {
+    brand: "LADA",
+    car_count: 430,
+    first_image: "",
+    id: 3,
+    models: [
+      { count: 17833, name: "4x4" },
+      { count: 29291, name: "Priora" },
+      { count: 13582, name: "2107" },
+      { count: 15509, name: "2114" },
+      { count: 12561, name: "2113" },
+      { count: 9819, name: "\u041e\u043a\u0430" }
+    ],
+    second_image: ""
+  },
+  {
+    brand: "BMW",
+    car_count: 749,
+    first_image: "",
+    id: 4,
+    models: [
+      { count: 19638, name: "e63" },
+      { count: 15663, name: "x6" },
+      { count: 19998, name: "m5" }
+    ],
+    second_image: ""
+  },
+  {
+    brand: "Mercedes",
+    car_count: 963,
+    first_image: "",
+    id: 5,
+    models: [
+      { count: 28404, name: "cls 63s" },
+      { count: 21228, name: "400" },
+      { count: 2697, name: "gle" }
+    ],
+    second_image: ""
+  }
+];
